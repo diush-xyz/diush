5e45895e19f1ed1012eb1e01734a9ee939aad9e6
filippo-fonseca <filifonsecacagnazzo@gmail.com>
2022-07-01 11:15:00 -0600

    proper tsconfig setup and expo

---
+++ core/components/Test.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+
+const Test = () => {
+  return (
+    <View>
+      <Text>Test</Text>
+    </View>
+  );
+};
+
+export default Test;
