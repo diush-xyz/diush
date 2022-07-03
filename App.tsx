1abe2f60cf2df0fc5111d9c3be4ac428f6d61a39
filippo-fonseca <filifonsecacagnazzo@gmail.com>
2022-07-02 19:23:03 -0600

    bg

--- App.tsx
+++ App.tsx
@@ -15,7 +15,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#0D0223',
     alignItems: 'center',
     justifyContent: 'center',
   },
