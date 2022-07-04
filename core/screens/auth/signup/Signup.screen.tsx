f18d8d3cabbfb41daf5abe96660599edd3e62d3e
filippo-fonseca <filifonsecacagnazzo@gmail.com>
2022-07-04 13:22:34 -0600

    rfct => 🎧 minor cleanup

--- core/screens/auth/signup/Signup.screen.tsx
+++ core/screens/auth/signup/Signup.screen.tsx
@@ -1,12 +1,22 @@
-import { View, Text } from "react-native";
+import { View, Text, TouchableOpacity } from "react-native";
 import React from "react";
+import { useAuthStore } from "../../../state/auth/Auth.store";
+import { AuthStatus } from "../../../@types/GlobalTypes";
+import CustomText from "../../../components/lib/CustomText";
+import { observer } from "mobx-react";
 
 const SignupScreen = () => {
+    const authStore = useAuthStore();
     return (
         <View>
             <Text>SignupScreen</Text>
+            <TouchableOpacity
+                onPress={() => authStore.setAuthStatus(AuthStatus.SQUARE_ONE)}
+            >
+                <CustomText primary>Back</CustomText>
+            </TouchableOpacity>
         </View>
     );
 };
 
-export default SignupScreen;
+export default observer(SignupScreen);
