5e82fdd8d377c809f3f5e96b882a9022a53d1fcf
filippo-fonseca <filifonsecacagnazzo@gmail.com>
2022-07-04 18:51:56 -0600

    initial child for flow element signup

--- core/screens/auth/signup/Signup.flow.tsx
+++ core/screens/auth/signup/Signup.flow.tsx
@@ -9,6 +9,7 @@ import { GLOBAL_STYLES } from "../../../@types/GlobalStyles";
 import PopupHeader from "../../../components/lib/PopupHeader";
 import CustomGradientCircle from "../../../components/auth/CustomGradientCircle";
 import FlowTemplate from "../../../components/lib/FlowTemplate/FlowTemplate.ui";
+import LargeButton from "../../../components/lib/LargeButton";
 
 const SignupFlow = () => {
     const authStore = useAuthStore();
@@ -24,7 +25,29 @@ const SignupFlow = () => {
                         "what’s most comfortable for you? we \n promise this will be quick (<2min)."
                     }
                 >
-                    <Text>The best</Text>
+                    <TouchableOpacity
+                        style={{
+                            height: 44,
+                            width: 330,
+                            display: "flex",
+                            justifyContent: "center",
+                            alignSelf: "center",
+                            borderRadius: 22,
+                            padding: 2,
+                            // make the background have 5% opacity from white
+                            backgroundColor: "rgba(255,255,255,0.05)",
+                        }}
+                    >
+                        <View
+                            style={{
+                                display: "flex",
+                                justifyContent: "center",
+                                alignItems: "center",
+                            }}
+                        >
+                            <CustomText primary>continue with email</CustomText>
+                        </View>
+                    </TouchableOpacity>
                 </FlowTemplate>
             </BottomSheetView>
         </>
