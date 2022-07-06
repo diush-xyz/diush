e0daa040a58025b432f2979d09933fef1b1ed266
filippo-fonseca <filifonsecacagnazzo@gmail.com>
2022-07-05 22:18:55 -0600

    rfct => 🎧 minor cleanup

--- core/components/auth/signup/email/EmailSignup.tsx
+++ core/components/auth/signup/email/EmailSignup.tsx
@@ -14,6 +14,7 @@ import CustomTextInput from "../../../lib/CustomTextInput";
 const EmailSignup = () => {
     const signupStore = useSignupStore();
     const theme = useTheme();
+
     return (
         <BottomSheetView style={GLOBAL_STYLES.bottomSheetViewStyle}>
             <PopupHeader
@@ -35,6 +36,7 @@ const EmailSignup = () => {
                     onChangeText={text => null}
                     marginBottom={32}
                 />
+                {/*//TODO: Add disabled validation with text field*/}
                 <LargeButton
                     title="continue"
                     onPress={() => null}
