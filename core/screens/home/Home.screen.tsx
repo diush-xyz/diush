7407134b8ac536ed0df1007d03ae8b831f40a562
filippo-fonseca <filifonsecacagnazzo@gmail.com>
2022-07-09 21:58:38 -0600

    proper log out

--- core/screens/home/Home.screen.tsx
+++ core/screens/home/Home.screen.tsx
@@ -3,14 +3,17 @@ import React from "react";
 import { observer } from "mobx-react";
 import { useAuthStore } from "../../state/auth/Auth.store";
 import CustomText from "../../components/lib/CustomText";
+import LargeButton from "../../components/lib/LargeButton";
+import { auth } from "../../../config/firebase";
 
 const HomeScreen = () => {
     const authStore = useAuthStore();
     return (
-        <View>
-            <CustomText primary>
+        <View style={{ width: "100%" }}>
+            <CustomText primary textAlign="center">
                 Welcome, {authStore?.user?.displayName}
             </CustomText>
+            <LargeButton title="Log out" onPress={() => auth.signOut()} />
         </View>
     );
 };
