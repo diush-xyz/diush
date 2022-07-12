3358b5782fa82fee04e3bd316d2a3c92ad48893a
filippo-fonseca <filifonsecacagnazzo@gmail.com>
2022-07-12 09:10:18 -0600

    ft[login/Email] => 🚀 Email screen and store connection

--- core/components/auth/login/EmailLogin.tsx
+++ core/components/auth/login/EmailLogin.tsx
@@ -4,17 +4,17 @@ import { BottomSheetView } from "@gorhom/bottom-sheet";
 import KeyboardListener from "react-native-keyboard-listener";
 import { observer } from "mobx-react";
 import { GLOBAL_STYLES } from "../../../@types/GlobalStyles";
-import { useSignupStore } from "../../../state/auth/Signup.store";
 import { useUtilStore } from "../../../state/Util.store";
 import CustomTextInput from "../../lib/CustomTextInput";
 import FlowTemplate from "../../lib/FlowTemplate";
 import LargeButton from "../../lib/LargeButton";
 import PopupHeader from "../../lib/PopupHeader";
+import { useLoginStore } from "../../../state/auth/Login.store";
 
 //TODO: Animation for keyboardShow FlowTemplate margin-bottom
 
 const EmailLogin = () => {
-    const signupStore = useSignupStore();
+    const loginStore = useLoginStore();
     const utilStore = useUtilStore();
     const [firstTime, setFirstTime] = React.useState<boolean>(true);
     const [allClear, setAllClear] = React.useState(false);
@@ -24,41 +24,37 @@ const EmailLogin = () => {
         //TODO: Come back later (two-letter domain extensions do not work, neither does .ed.cr,)
         // setIsReady(validateEmail(signupStore.email));
 
-        if (signupStore.email === "" || !signupStore.email.includes("@")) {
+        if (loginStore.email === "" || !loginStore.email.includes("@")) {
             setAllClear(false);
             setErrMsg("oop! you need a valid email address to continue.");
         } else {
             setAllClear(true);
         }
 
-        console.log("the email: " + signupStore.email);
+        console.log("the email: " + loginStore.email);
         console.log(allClear);
     });
 
     return (
         <BottomSheetView style={GLOBAL_STYLES.bottomSheetViewStyle}>
             <PopupHeader
-                backArrow
-                backArrowOnPress={() =>
-                    signupStore.setCurrentStep(signupStore.currentStep - 1)
-                }
                 title="email"
                 subtitle="login"
                 progressIndicator
-                currentStep={2}
-                totalSteps={5}
+                currentStep={1}
+                totalSteps={2}
             />
             <FlowTemplate
-                circleEmoji="✉️"
-                title="email"
-                desc={"please enter your email address below."}
+                circleEmoji="👋"
+                title="welcome back!"
+                desc={"we're trhilled you're here. you've been missed."}
                 marginBottom={utilStore.isKeyboardOpen ? "200px" : null}
             >
                 <CustomTextInput
                     placeholder="my email"
-                    onChangeText={text => signupStore.setEmail(text)}
+                    onChangeText={text => loginStore.setEmail(text)}
                     marginBottom={32}
-                    defaultValue={signupStore.email}
+                    defaultValue={loginStore.email}
                     keyboardType="email-address"
                     isValid={allClear}
                     isErr={!allClear && !firstTime}
@@ -70,15 +66,15 @@ const EmailLogin = () => {
                     onPress={() => {
                         setFirstTime(false);
                         if (allClear) {
-                            signupStore.setCurrentStep(
-                                signupStore.currentStep + 1
+                            loginStore.setCurrentStep(
+                                loginStore.currentStep + 1
                             );
                         }
                     }}
                     footer
                     disabled={!allClear && !firstTime}
                     footerButtonTitle="cancel"
-                    footerButtonOnPress={() => signupStore.cancel()}
+                    footerButtonOnPress={() => loginStore.cancel()}
                 />
             </FlowTemplate>
         </BottomSheetView>
