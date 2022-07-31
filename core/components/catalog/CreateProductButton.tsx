9e8e6bdb9795ad0d8a1688534622687595d01327
filippo-fonseca <filifonsecacagnazzo@gmail.com>
2022-07-30 19:15:51 -0600

    fx[catalog/CreateProductButton] => 🍀 Create func in actual button

--- core/components/catalog/CreateProductButton.tsx
+++ core/components/catalog/CreateProductButton.tsx
@@ -3,11 +3,15 @@ import React from "react";
 import { useTheme } from "../../utils/useTheme.util";
 import styled from "styled-components/native";
 import WandIcon from "../../icons/catalog/Wand";
+import { useCatalogStore } from "../../state/auth/Catalog.store";
+import { CatalogStatus } from "../../@types/GlobalTypes";
+import { observer } from "mobx-react";
 
 const CreateProductButton = () => {
+    const catalogStore = useCatalogStore();
     const theme = useTheme();
 
-    const Wrapper = styled.View`
+    const Wrapper = styled.TouchableOpacity`
         position: absolute;
         bottom: 41px;
         right: 20px;
@@ -19,10 +23,28 @@ const CreateProductButton = () => {
     `;
 
     return (
-        <Wrapper style={{ borderRadius: "50%" }}>
+        <Wrapper
+            style={{ borderRadius: "50%" }}
+            onPress={() => {
+                //     createProductInDb({
+                //         id: "yuyu",
+                //         linkedUID: auth.currentUser?.uid,
+                //         title: "JBL Speakers",
+                //         blurb: "Beautiful, compact, and powerful.",
+                //         askingPrice: 89.99,
+                //         imageURL:
+                //             "https://cdn.vox-cdn.com/thumbor/80TKHF3cpG3fg11Gb3bejNDkbIk=/1400x1400/filters:format(jpeg)/cdn.vox-cdn.com/uploads/chorus_asset/file/23382986/cwelch_220410_5136_0003.jpg",
+                //     })
+                //         .then(() => console.log("Created product in DB!"))
+                //         .catch(err =>
+                //             console.error("something went wrong: " + err)
+                //         );
+                catalogStore.setStatus(CatalogStatus.CREATE);
+            }}
+        >
             <WandIcon />
         </Wrapper>
     );
 };
 
-export default CreateProductButton;
+export default observer(CreateProductButton);
--- core/components/catalog/Home.tsx
+++ core/components/catalog/Home.tsx
@@ -135,25 +135,6 @@ const CatalogHome = () => {
                 title="my catalog"
             />
             <Switcher />
-            <LargeButton
-                title="Create a product"
-                onPress={() => {
-                    //     createProductInDb({
-                    //         id: "yuyu",
-                    //         linkedUID: auth.currentUser?.uid,
-                    //         title: "JBL Speakers",
-                    //         blurb: "Beautiful, compact, and powerful.",
-                    //         askingPrice: 89.99,
-                    //         imageURL:
-                    //             "https://cdn.vox-cdn.com/thumbor/80TKHF3cpG3fg11Gb3bejNDkbIk=/1400x1400/filters:format(jpeg)/cdn.vox-cdn.com/uploads/chorus_asset/file/23382986/cwelch_220410_5136_0003.jpg",
-                    //     })
-                    //         .then(() => console.log("Created product in DB!"))
-                    //         .catch(err =>
-                    //             console.error("something went wrong: " + err)
-                    //         );
-                    catalogStore.setStatus(CatalogStatus.CREATE);
-                }}
-            />
             <CustomTextInput
                 placeholder="search my products"
                 onChangeText={() => null}
