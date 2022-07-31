49365ff978637c59fdfb696112ec2fd54c4c3b49
filippo-fonseca <filifonsecacagnazzo@gmail.com>
2022-07-30 20:06:51 -0600

    rfct => 🎧 minor cleanup

--- core/components/catalog/create/AskingPrice.tsx
+++ core/components/catalog/create/AskingPrice.tsx
@@ -6,42 +6,90 @@ import { observer } from "mobx-react";
 import { GLOBAL_STYLES } from "../../../@types/GlobalStyles";
 import ProductCard from "../ProductCard";
 import CustomText from "../../lib/CustomText";
+import ScreenHeader from "../../lib/ScreenHeader";
+import FlowTemplate from "../../lib/FlowTemplate";
+import CustomTextInput from "../../lib/CustomTextInput";
 
 const AskingPrice = () => {
     const createProductStore = useCreateProductStore();
     const NUM_PAD_DATA = [1, 2, 3, 4, 5, 6, 7, 8, 9, ".", 0, "del"];
     return (
-        <View style={GLOBAL_STYLES.page}>
-            <FlatList
-                data={NUM_PAD_DATA}
-                numColumns={3}
-                columnWrapperStyle={{
-                    justifyContent: "space-between",
-                    marginBottom: 15,
-                }}
-                renderItem={({ item, index }) => (
-                    <View style={{ height: 25, width: 25 }}>
-                        <CustomText secondary fontSize={22} textAlign="center">
-                            {item}
-                        </CustomText>
-                    </View>
-                )}
-                style={{ width: "100%", maxWidth: 247 }}
+        <View
+            style={{
+                alignItems: "center",
+                flex: 1,
+                marginTop: 55,
+                width: "100%",
+            }}
+        >
+            <ScreenHeader
+                title="create listing"
+                backArrow
+                backArrowOnPress={() =>
+                    createProductStore.setCurrentStep(
+                        createProductStore.currentStep - 1
+                    )
+                }
+                subtitle="my catalog"
+                progressIndicator
+                currentStep={1}
+                totalSteps={8}
             />
-            <LargeButton
-                title="continue"
-                onPress={() => {
-                    // setFirstTime(false);
-                    // if (allClear) {
-                    //     createProductStore.setCurrentStep(1);
-                    // }
-                    null;
-                }}
-                footer
-                // disabled={!allClear && !firstTime}
-                footerButtonTitle="cancel"
-                footerButtonOnPress={() => createProductStore.cancel()}
-            />
+            <View style={{ display: "flex", marginTop: 32, width: "100%" }}>
+                <FlowTemplate
+                    circleEmoji="📦"
+                    title="how much are you seeking"
+                    desc={
+                        "this is the starting point for people\n when they come across your listing."
+                    }
+                >
+                    <CustomText
+                        font="Heavy"
+                        fontSize={56}
+                        style={{ marginBottom: 45 }}
+                    >
+                        $150
+                    </CustomText>
+                    <FlatList
+                        data={NUM_PAD_DATA}
+                        numColumns={3}
+                        columnWrapperStyle={{
+                            justifyContent: "space-between",
+                            marginBottom: 15,
+                        }}
+                        renderItem={({ item, index }) => (
+                            <View style={{ height: 25, width: 25 }}>
+                                <CustomText
+                                    secondary
+                                    fontSize={22}
+                                    textAlign="center"
+                                >
+                                    {item}
+                                </CustomText>
+                            </View>
+                        )}
+                        style={{ width: "100%", maxWidth: 247 }}
+                    />
+                    <LargeButton
+                        title="continue"
+                        onPress={() => {
+                            // setFirstTime(false);
+                            // if (allClear) {
+                            //     createProductStore.setCurrentStep(1);
+                            // }
+                            null;
+                        }}
+                        footer
+                        // disabled={!allClear && !firstTime}
+                        footerButtonTitle="cancel"
+                        footerButtonOnPress={() =>
+                            createProductStore.setCurrentStep(
+                                createProductStore.currentStep - 1
+                            )
+                        }
+                    />
+                </FlowTemplate>
+            </View>
         </View>
     );
 };
