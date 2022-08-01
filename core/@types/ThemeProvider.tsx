eab05c21998d909e60f105ad7775b77df4e64927
filippo-fonseca <filifonsecacagnazzo@gmail.com>
2022-07-31 19:21:26 -0600

    fx[catalog/ConditionModal] => 🎹 Proper additions

--- core/@types/ThemeProvider.tsx
+++ core/@types/ThemeProvider.tsx
@@ -5,16 +5,19 @@ type PossibleTheme = "dark" | "light";
 export interface ITheme {
     readonly themeName: PossibleTheme;
     background: string;
+    popupBackground: string;
 }
 
 // Light Mode (default):
 export const LightTheme: ITheme = {
     themeName: "light",
     background: "#ffffff",
+    popupBackground: "red",
 };
 
 // Dark Mode:
 export const DarkTheme: ITheme = {
     themeName: "dark",
     background: "#171717",
+    popupBackground: "#13052F",
 };
--- core/components/catalog/ConditionModal/ConditionModal.tsx
+++ core/components/catalog/ConditionModal/ConditionModal.tsx
@@ -1,36 +1,67 @@
 import React, { useState } from "react";
-import { Alert, Modal, StyleSheet, Text, Pressable, View } from "react-native";
+import {
+    Alert,
+    Modal,
+    StyleSheet,
+    Text,
+    Pressable,
+    View,
+    TouchableWithoutFeedback,
+    TouchableOpacity,
+} from "react-native";
+import InfoIcon from "../../../icons/common/info";
+import { useTheme } from "../../../utils/useTheme.util";
+import CustomText from "../../lib/CustomText";
+import ModalElement from "./ModalElement";
 
 interface IConditionModal {
     modalVisible: boolean;
     setModalVisible: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
+const theme = useTheme();
+
 const ConditionModal = (props: IConditionModal) => {
     return (
         <View style={styles.centeredView}>
             <Modal
                 animationType="slide"
-                transparent={true}
+                transparent
                 visible={props.modalVisible}
                 onRequestClose={() => {
                     Alert.alert("Modal has been closed.");
                     props.setModalVisible(!props.modalVisible);
                 }}
             >
-                <View style={styles.centeredView}>
-                    <View style={styles.modalView}>
-                        <Text style={styles.modalText}>Hello World!</Text>
-                        <Pressable
-                            style={[styles.button, styles.buttonClose]}
-                            onPress={() =>
-                                props.setModalVisible(!props.modalVisible)
-                            }
-                        >
-                            <Text style={styles.textStyle}>Hide Modal</Text>
-                        </Pressable>
+                <TouchableWithoutFeedback
+                    onPress={() => props.setModalVisible(!props.modalVisible)}
+                >
+                    <View style={styles.centeredView}>
+                        <View style={styles.modalView}>
+                            <ModalElement
+                                text="new with box"
+                                onPress={() => console.log("Clicked!")}
+                            />
+                            <ModalElement
+                                text="new without box"
+                                onPress={() => console.log("Clicked!")}
+                            />
+                            <ModalElement
+                                text="new with defects"
+                                onPress={() => console.log("Clicked!")}
+                            />
+                            <ModalElement
+                                text="used (good condition)"
+                                onPress={() => console.log("Clicked!")}
+                            />
+                            <ModalElement
+                                text="used (decent condition)"
+                                onPress={() => console.log("Clicked!")}
+                                noBorderBottom
+                            />
+                        </View>
                     </View>
-                </View>
+                </TouchableWithoutFeedback>
             </Modal>
         </View>
     );
@@ -42,21 +73,25 @@ const styles = StyleSheet.create({
         justifyContent: "center",
         alignItems: "center",
         marginTop: 22,
+        backgroundColor: "rgba(0, 0, 0, 0.6)",
     },
     modalView: {
+        alignItems: "flex-start",
+        justifyContent: "flex-start",
         margin: 20,
-        backgroundColor: "white",
-        borderRadius: 20,
-        padding: 35,
-        alignItems: "center",
+        backgroundColor: theme.popupBackground,
+        borderRadius: 16,
         shadowColor: "#000",
         shadowOffset: {
             width: 0,
             height: 2,
         },
+        borderColor: theme.secondary,
+        borderWidth: 1,
         shadowOpacity: 0.25,
         shadowRadius: 4,
         elevation: 5,
+        width: 242,
     },
     button: {
         borderRadius: 20,
