d36210f3ad2fb470fd52a0b56a875feb2c439367
filippo-fonseca <filifonsecacagnazzo@gmail.com>
2022-08-01 21:35:03 -0600

    ft[catalog/CreateProductFlow] => 🥮 CREATE PRODUCT FLOW DONE (INITIAL)

--- core/components/catalog/create/ProductReview.tsx
+++ core/components/catalog/create/ProductReview.tsx
@@ -1,4 +1,4 @@
-import { View, Text, FlatList } from "react-native";
+import { View, Text, FlatList, Image } from "react-native";
 import React from "react";
 import LargeButton from "../../lib/LargeButton";
 import { useCreateProductStore } from "../../../state/auth/CreateProduct.store";
@@ -16,8 +16,13 @@ import PopupHeader from "../../lib/PopupHeader";
 import { TouchableOpacity } from "react-native-gesture-handler";
 import { useCatalogStore } from "../../../state/auth/Catalog.store";
 import { CatalogStatus } from "../../../@types/GlobalTypes";
+import { useTheme } from "../../../utils/useTheme.util";
+import { LinearTextGradient } from "react-native-text-gradient";
+import { createProductInDb } from "../../../utils/products.util";
+import { auth } from "../../../../config/firebase";
 
 const ProductReview = () => {
+    const theme = useTheme();
     const catalogStore = useCatalogStore();
     const createProductStore = useCreateProductStore();
     const [price, setPrice] = React.useState<string>(
@@ -47,24 +52,97 @@ const ProductReview = () => {
                         "would you like to add this item to\n your catalog? this will allow you to\n share its link with your friends."
                     }
                 >
+                    <View
+                        style={{
+                            padding: 12,
+                            borderRadius: 12,
+                            backgroundColor: "rgba(255, 255, 255, 0.05)",
+                            marginBottom: 32,
+                        }}
+                    >
+                        <View
+                            style={{
+                                justifyContent:
+                                    createProductStore.productImageURL == ""
+                                        ? "center"
+                                        : null,
+                                width: 300,
+                                height: 166,
+                                borderStyle: "dashed",
+                                borderWidth: 1,
+                                borderRadius: 12,
+                                borderColor: theme.secondary,
+                                marginBottom: 23,
+                            }}
+                        >
+                            <Image
+                                style={{
+                                    flex: 1,
+                                    resizeMode: "cover",
+                                    justifyContent: "center",
+                                    borderRadius: 12,
+                                }}
+                                source={{
+                                    uri: createProductStore.productImageURL,
+                                }}
+                            />
+                        </View>
+                        <CustomText
+                            font="Heavy"
+                            fontSize={22}
+                            style={{ marginBottom: 6, letterSpacing: 1 }}
+                        >
+                            {createProductStore.productName}
+                        </CustomText>
+                        <CustomText
+                            accent
+                            font="Heavy"
+                            fontSize={18}
+                            style={{ marginBottom: 12 }}
+                        >
+                            ${createProductStore.askingPrice}
+                        </CustomText>
+                        <CustomText
+                            secondary
+                            font="Bold"
+                            style={{ marginBottom: 5 }}
+                        >
+                            {createProductStore.blurb}
+                        </CustomText>
+                        <CustomText secondary font="Heavy">
+                            {createProductStore.condition}
+                        </CustomText>
+                    </View>
                     <LargeButton
                         title="post"
                         onPress={() => {
-                            const parsedPrice = Number(price);
-                            if (parsedPrice > 0) {
-                                createProductStore.setAskingPrice(parsedPrice);
-                                createProductStore.setCurrentStep(
-                                    createProductStore.currentStep + 1
+                            createProductInDb({
+                                id: createProductStore.productId,
+                                linkedUID: auth.currentUser?.uid,
+                                title: createProductStore.productName,
+                                blurb: createProductStore.blurb,
+                                askingPrice: createProductStore.askingPrice,
+                                imageURL: createProductStore.productImageURL,
+                            })
+                                .then(() => {
+                                    console.log("Created product in DB!");
+                                    createProductStore.cancel();
+                                    catalogStore.setStatus(
+                                        CatalogStatus.ACTIVE_DASH
+                                    );
+                                })
+                                .catch(err =>
+                                    console.error(
+                                        "something went wrong: " + err
+                                    )
                                 );
-                            }
                         }}
                         footer
                         // disabled={Number(price) <= 0}
                         // disabled={!allClear && !firstTime}
-                        footerButtonTitle="cancel"
+                        footerButtonTitle="back to editing"
                         footerButtonOnPress={() => {
-                            createProductStore.cancel();
-                            catalogStore.setStatus(CatalogStatus.ACTIVE_DASH);
+                            createProductStore.setCurrentStep(0);
                         }}
                     />
                 </FlowTemplate>
