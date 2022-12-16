74c7482ce6b904c73900396fc81d3c0f669c34c6
filippo-fonseca <filifonsecacagnazzo@gmail.com>
2022-12-16 11:35:17 -0600

    update deps

--- core/components/catalog/viewProduct/ViewProduct.tsx
+++ core/components/catalog/viewProduct/ViewProduct.tsx
@@ -5,7 +5,11 @@ import FlowTemplate from "../../lib/FlowTemplate";
 import PopupHeader from "../../lib/PopupHeader";
 import { observer } from "mobx-react";
 import { useSignupStore } from "../../../state/auth/Signup.store";
-import { CatalogStatus, SignupMethod } from "../../../@types/GlobalTypes";
+import {
+    CatalogStatus,
+    IUser,
+    SignupMethod,
+} from "../../../@types/GlobalTypes";
 import SignupOptionButton from "../../auth/SignupOptionbutton/SignupOptionButton";
 import { View } from "react-native";
 import { useCreateProductStore } from "../../../state/auth/CreateProduct.store";
@@ -19,6 +23,9 @@ import { Image } from "react-native";
 import ImageOverlay from "./ImageOverlay";
 import CarouselIcon from "../../../icons/catalog/Carousel";
 import ShareIcon from "../../../icons/catalog/Share";
+import { useAuthStore } from "../../../state/auth/Auth.store";
+import { fetchUserFromDb } from "../../../utils/user.utils";
+import { auth } from "../../../../config/firebase";
 
 const ViewProduct = () => {
     const catalogStore = useCatalogStore();
@@ -27,21 +34,19 @@ const ViewProduct = () => {
     const [firstTime, setFirstTime] = React.useState<boolean>(true);
     const [allClear, setAllClear] = React.useState(false);
     const [errMsg, setErrMsg] = React.useState<string>("");
+    const authStore = useAuthStore();
+    const [fetchedUser, setFetchedUser] = React.useState<IUser>();
 
     React.useEffect(() => {
         //TODO: Come back later (two-letter domain extensions do not work, neither does .ed.cr,)
         // setIsReady(validateEmail(signupStore.email));
-
-        if (createProductStore.productName === "") {
-            setAllClear(false);
-            setErrMsg("oop! your product needs a name.");
-        } else {
-            setAllClear(true);
-        }
-
-        console.log("the name: " + createProductStore.productName);
-        console.log(allClear);
-    });
+        fetchUserFromDb({
+            id: auth.currentUser?.uid,
+            setUser: (user: IUser) => {
+                setFetchedUser(user);
+            },
+        });
+    }, []);
 
     return (
         <BottomSheetView style={GLOBAL_STYLES.viewProductSheetViewStyle}>
@@ -62,6 +67,7 @@ const ViewProduct = () => {
                         display: "flex",
                         flexDirection: "row",
                         justifyContent: "space-between",
+                        marginBottom: 14,
                     }}
                 >
                     <View style={{ display: "flex", flexDirection: "row" }}>
@@ -90,6 +96,10 @@ const ViewProduct = () => {
                 <CustomText font="Heavy" fontSize={22}>
                     {catalogStore.activeProduct.title}
                 </CustomText>
+                <Image
+                    source={{ uri: fetchedUser?.photoURL }}
+                    style={{ height: 20, width: 20 }}
+                />
             </View>
         </BottomSheetView>
     );
