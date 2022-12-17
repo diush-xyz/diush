09a939a085f51ffbc014409bd5a90c475baf39ee
filippo-fonseca <filifonsecacagnazzo@gmail.com>
2022-12-17 12:45:45 -0600

    proper product view scroll

--- core/components/catalog/viewProduct/ViewProduct.tsx
+++ core/components/catalog/viewProduct/ViewProduct.tsx
@@ -33,6 +33,7 @@ import OfferButton from "./OfferButton";
 import RoundedMoreIcon from "../../../icons/catalog/RoundedMore";
 import ActiveIndicator from "./ActiveIndicator";
 import SnapshotBox from "./SnapshotBox";
+import ProductViewScrollWrapper from "./ProductViewScrollWrapper";
 
 const ViewProduct = () => {
     const catalogStore = useCatalogStore();
@@ -58,119 +59,143 @@ const ViewProduct = () => {
 
     return (
         <BottomSheetView style={GLOBAL_STYLES.viewProductSheetViewStyle}>
-            <Image
-                style={{
-                    height: 471,
-                    width: "100%",
-                    resizeMode: "cover",
-                }}
-                source={{
-                    uri: catalogStore.activeProduct.imageURL,
-                }}
-            />
-            <ImageOverlay style={{ position: "absolute", top: 0 }} />
-            <View style={{ marginTop: -185, paddingHorizontal: 22 }}>
-                <View
+            <ProductViewScrollWrapper>
+                <Image
                     style={{
-                        display: "flex",
-                        flexDirection: "row",
-                        justifyContent: "space-between",
-                        marginBottom: 14,
-                    }}
-                >
-                    <View style={{ display: "flex", flexDirection: "row" }}>
-                        <CarouselIcon />
-                        <CustomText
-                            accent
-                            font="Heavy"
-                            fontSize={16}
-                            style={{ marginLeft: 6 }}
-                        >
-                            view carousel
-                        </CustomText>
-                    </View>
-                    <View style={{ display: "flex", flexDirection: "row" }}>
-                        <ShareIcon />
-                        <CustomText
-                            accent
-                            font="Heavy"
-                            fontSize={16}
-                            style={{ marginLeft: 6 }}
-                        >
-                            share
-                        </CustomText>
-                    </View>
-                </View>
-                <View
-                    style={{
-                        display: "flex",
-                        flexDirection: "row",
-                        alignItems: "center",
-                        justifyContent: "space-between",
+                        height: 471,
                         width: "100%",
+                        resizeMode: "cover",
                     }}
-                >
-                    <CustomText font="Heavy" fontSize={22}>
-                        {catalogStore.activeProduct.title}
-                    </CustomText>
-                    <RoundedMoreIcon />
-                </View>
-                <View
-                    style={{
-                        display: "flex",
-                        flexDirection: "row",
-                        marginTop: 10,
-                        alignItems: "center",
+                    source={{
+                        uri: catalogStore.activeProduct.imageURL,
                     }}
-                >
-                    <Image
-                        borderRadius={10}
-                        source={{
-                            uri:
-                                fetchedUser?.photoURL ??
-                                "https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dXNlciUyMHByb2ZpbGV8ZW58MHx8MHx8&w=1000&q=80",
-                        }}
+                />
+                <ImageOverlay style={{ position: "absolute", top: 0 }} />
+                <View style={{ marginTop: -185, paddingHorizontal: 22 }}>
+                    <View
                         style={{
-                            height: 20,
-                            width: 20,
+                            display: "flex",
+                            flexDirection: "row",
+                            justifyContent: "space-between",
+                            marginBottom: 14,
+                        }}
+                    >
+                        <View style={{ display: "flex", flexDirection: "row" }}>
+                            <CarouselIcon />
+                            <CustomText
+                                accent
+                                font="Heavy"
+                                fontSize={16}
+                                style={{ marginLeft: 6 }}
+                            >
+                                view carousel
+                            </CustomText>
+                        </View>
+                        <View style={{ display: "flex", flexDirection: "row" }}>
+                            <ShareIcon />
+                            <CustomText
+                                accent
+                                font="Heavy"
+                                fontSize={16}
+                                style={{ marginLeft: 6 }}
+                            >
+                                share
+                            </CustomText>
+                        </View>
+                    </View>
+                    <View
+                        style={{
+                            display: "flex",
+                            flexDirection: "row",
+                            alignItems: "center",
+                            justifyContent: "space-between",
+                            width: "100%",
+                        }}
+                    >
+                        <CustomText font="Heavy" fontSize={22}>
+                            {catalogStore.activeProduct.title}
+                        </CustomText>
+                        <RoundedMoreIcon />
+                    </View>
+                    <View
+                        style={{
+                            display: "flex",
+                            flexDirection: "row",
+                            marginTop: 10,
+                            alignItems: "center",
+                        }}
+                    >
+                        <Image
+                            borderRadius={10}
+                            source={{
+                                uri:
+                                    fetchedUser?.photoURL ??
+                                    "https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dXNlciUyMHByb2ZpbGV8ZW58MHx8MHx8&w=1000&q=80",
+                            }}
+                            style={{
+                                height: 20,
+                                width: 20,
+                            }}
+                        />
+                        <CustomText
+                            fontSize={16}
+                            style={{ marginLeft: 6 }}
+                            font="Bold"
+                        >
+                            <CustomText font="Bold" style={{ opacity: 0.5 }}>
+                                listed by
+                            </CustomText>{" "}
+                            me
+                        </CustomText>
+                        <ChevronRight style={{ marginLeft: 7 }} />
+                    </View>
+                    <View
+                        style={{
+                            display: "flex",
+                            flexDirection: "row",
+                            marginTop: 10,
+                            alignItems: "center",
+                        }}
+                    >
+                        <TicketIcon />
+                        <CustomText
+                            accent
+                            font="Bold"
+                            style={{ marginLeft: 2 }}
+                        >
+                            5 orders total
+                        </CustomText>
+                        <ActiveIndicator />
+                        {/*TODO: Add shadow!!*/}
+                    </View>
+                    <View style={{ marginTop: 20 }}>
+                        <OfferButton title="view offers" onPress={() => null} />
+                    </View>
+                    <SnapshotBox
+                        askingPrice={585}
+                        highestOffer={600}
+                        posted="14h"
+                    />
+                    <View
+                        style={{
+                            width: "100%",
+                            height: 1,
+                            backgroundColor: "#ffffff0D",
+                            marginVertical: 30,
                         }}
                     />
                     <CustomText
-                        fontSize={16}
-                        style={{ marginLeft: 6 }}
-                        font="Bold"
+                        fontSize={18}
+                        font="Heavy"
+                        style={{ marginBottom: 14 }}
                     >
-                        <CustomText font="Bold" style={{ opacity: 0.5 }}>
-                            listed by
-                        </CustomText>{" "}
-                        me
+                        blurb
                     </CustomText>
-                    <ChevronRight style={{ marginLeft: 7 }} />
-                </View>
-                <View
-                    style={{
-                        display: "flex",
-                        flexDirection: "row",
-                        marginTop: 10,
-                        alignItems: "center",
-                    }}
-                >
-                    <TicketIcon />
-                    <CustomText accent font="Bold" style={{ marginLeft: 2 }}>
-                        5 orders total
+                    <CustomText secondary>
+                        {catalogStore.activeProduct.blurb}
                     </CustomText>
-                    <ActiveIndicator />
-                    {/*TODO: Add shadow!!*/}
                 </View>
-                <View style={{ marginTop: 20 }}>
-                    <OfferButton title="view offers" onPress={() => null} />
-                </View>
-                <SnapshotBox
-                    askingPrice={585}
-                    highestOffer={600}
-                    posted="14h"
-                />
-            </View>
+            </ProductViewScrollWrapper>
         </BottomSheetView>
     );
 };
