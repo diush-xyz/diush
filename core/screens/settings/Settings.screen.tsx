40ceed2ff74644ecfb0dfdf6bd9aa167a98c90d1
filippo-fonseca <filifonsecacagnazzo@gmail.com>
2023-01-17 16:17:25 -0600

    proper settings screen improv

--- core/screens/settings/Settings.screen.tsx
+++ core/screens/settings/Settings.screen.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import CustomText from "../../components/lib/CustomText";
-import { TouchableOpacity, View } from "react-native";
+import { ScrollView, TouchableOpacity, View } from "react-native";
 import ScreenHeader from "../../components/lib/ScreenHeader";
 import { useAuthStore } from "../../state/auth/Auth.store";
 import { useHomeStore } from "../../state/auth/Home.store";
@@ -8,12 +8,23 @@ import CustomTextInput from "../../components/lib/CustomTextInput";
 import HorizontalLine from "../../components/lib/HorizontalLine";
 import { MAX_WIDTH } from "../../utils/constants";
 import ChevronRight from "../../icons/catalog/ChevronRight";
+import { auth } from "../../../config/firebase";
+import { useLoginStore } from "../../state/auth/Login.store";
+import { useSignupStore } from "../../state/auth/Signup.store";
+
+interface ISettingsData {
+    text: string;
+    onClick: () => void;
+    isToggle?: boolean;
+}
 
 const SettingsScreen = () => {
     const authStore = useAuthStore();
     const homeStore = useHomeStore();
+    const logInStore = useLoginStore();
+    const signUpStore = useSignupStore();
 
-    const PREFERENCES_SETTINGS_DATA = [
+    const PREFERENCES_SETTINGS_DATA: ISettingsData[] = [
         {
             text: "my account",
             onClick: () => null,
@@ -29,6 +40,32 @@ const SettingsScreen = () => {
         {
             text: "notifications",
             onClick: () => null,
+            isToggle: true,
+        },
+    ];
+
+    const CONNECT_SETTINGS_DATA: ISettingsData[] = [
+        {
+            text: "follow us on twitter 🐦",
+            onClick: () => null,
+        },
+        {
+            text: "rate us ⭐",
+            onClick: () => null,
+        },
+        {
+            text: "about 🎧",
+            onClick: () => null,
+        },
+        {
+            text: "help center 🌳",
+            onClick: () => null,
+            isToggle: true,
+        },
+        {
+            text: "give feedback ❤️",
+            onClick: () => null,
+            isToggle: true,
         },
     ];
 
@@ -54,87 +91,107 @@ const SettingsScreen = () => {
                 // }
                 title="settings"
             />
-            <View style={{ marginTop: 22 }}>
+            <View style={{ marginTop: 22, marginBottom: 22 }}>
                 <CustomTextInput
                     placeholder="search settings"
                     onChangeText={text => null}
                     isSearch
                 />
             </View>
-            <View
-                style={{
-                    display: "flex",
-                    width: MAX_WIDTH,
-                }}
+            <ScrollView
+                contentContainerStyle={{ width: MAX_WIDTH }}
+                showsVerticalScrollIndicator={false}
             >
-                <CustomText
-                    secondary
-                    font="Heavy"
-                    fontSize={14}
-                    style={{ marginTop: 22 }}
+                <View
+                    style={{
+                        display: "flex",
+                        width: "100%",
+                    }}
                 >
-                    PREFERENCES
-                </CustomText>
-                <HorizontalLine marginVertical={8} />
-                {PREFERENCES_SETTINGS_DATA.map((elem, idx) => {
-                    return (
-                        <>
-                            <TouchableOpacity
-                                onPress={elem.onClick}
-                                style={{
-                                    display: "flex",
-                                    flexDirection: "row",
-                                    width: "100%",
-                                    alignItems: "center",
-                                    justifyContent: "space-between",
-                                    marginTop: idx == 0 && 8,
-                                }}
-                            >
-                                <CustomText>{elem.text}</CustomText>
-                                <ChevronRight />
-                            </TouchableOpacity>
-                            <HorizontalLine marginVertical={16} />
-                        </>
-                    );
-                })}
-            </View>
-            <View
-                style={{
-                    display: "flex",
-                    width: MAX_WIDTH,
-                }}
-            >
-                <CustomText
-                    secondary
-                    font="Heavy"
-                    fontSize={14}
-                    style={{ marginTop: 22 }}
+                    <CustomText secondary font="Heavy" fontSize={14}>
+                        PREFERENCES
+                    </CustomText>
+                    <HorizontalLine marginVertical={8} />
+                    {PREFERENCES_SETTINGS_DATA.map((elem, idx) => {
+                        return (
+                            <>
+                                <TouchableOpacity
+                                    onPress={elem.onClick}
+                                    style={{
+                                        display: "flex",
+                                        flexDirection: "row",
+                                        width: "100%",
+                                        alignItems: "center",
+                                        justifyContent: "space-between",
+                                        marginTop: idx == 0 && 8,
+                                    }}
+                                >
+                                    <CustomText>{elem.text}</CustomText>
+                                    {elem.isToggle ? (
+                                        <CustomText>tg</CustomText>
+                                    ) : (
+                                        <ChevronRight />
+                                    )}
+                                </TouchableOpacity>
+                                <HorizontalLine marginVertical={16} />
+                            </>
+                        );
+                    })}
+                </View>
+                <View
+                    style={{
+                        display: "flex",
+                        width: "100%",
+                    }}
                 >
-                    CONNECT
-                </CustomText>
-                <HorizontalLine marginVertical={8} />
-                {PREFERENCES_SETTINGS_DATA.map((elem, idx) => {
-                    return (
-                        <>
-                            <TouchableOpacity
-                                onPress={elem.onClick}
-                                style={{
-                                    display: "flex",
-                                    flexDirection: "row",
-                                    width: "100%",
-                                    alignItems: "center",
-                                    justifyContent: "space-between",
-                                    marginTop: idx == 0 && 8,
-                                }}
-                            >
-                                <CustomText>{elem.text}</CustomText>
-                                <ChevronRight />
-                            </TouchableOpacity>
-                            <HorizontalLine marginVertical={16} />
-                        </>
-                    );
-                })}
-            </View>
+                    <CustomText
+                        secondary
+                        font="Heavy"
+                        fontSize={14}
+                        style={{ marginTop: 22 }}
+                    >
+                        CONNECT
+                    </CustomText>
+                    <HorizontalLine marginVertical={8} />
+                    {CONNECT_SETTINGS_DATA.map((elem, idx) => {
+                        return (
+                            <>
+                                <TouchableOpacity
+                                    onPress={elem.onClick}
+                                    style={{
+                                        display: "flex",
+                                        flexDirection: "row",
+                                        width: "100%",
+                                        alignItems: "center",
+                                        justifyContent: "space-between",
+                                        marginTop: idx == 0 && 8,
+                                    }}
+                                >
+                                    <CustomText>{elem.text}</CustomText>
+                                    <ChevronRight />
+                                </TouchableOpacity>
+                                <HorizontalLine marginVertical={16} />
+                            </>
+                        );
+                    })}
+                    <TouchableOpacity
+                        onPress={() => {
+                            auth.signOut();
+                            //TODO: still must fix blank popup that comes up after the user signs out of their acc
+                            //reset stuff
+                            homeStore.setControlCenter(false);
+                            logInStore.cancel();
+                            signUpStore.cancel();
+                            homeStore.setIsIncomingChatsActive(true);
+                            homeStore.setIsOutboundChatsActive(false);
+                            homeStore.setControlCenter(false);
+                        }}
+                    >
+                        <CustomText accent>log out</CustomText>
+                    </TouchableOpacity>
+                </View>
+                <View style={{ marginBottom: 100 }} />
+            </ScrollView>
         </View>
     );
 };
